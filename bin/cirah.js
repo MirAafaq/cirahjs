affee83fe620846ea0b11258f83dc34fc161b22e
MirAafaq <mira77065@gmail.com>
2024-06-19 14:48:07 +0530

    stable after v1.0.0

--- bin/cirah.js
+++ bin/cirah.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { moveFiles, logMessage } = require('../lib/index');
+const { moveFiles, logMessage, undoLastMove, colorize } = require('../lib/index');
 const chalk = require('chalk');
 const readline = require('readline');
 const fs = require('fs');
@@ -12,7 +12,6 @@ const rl = readline.createInterface({
 });
 
 const logFile = 'audit.log';
-
 function main() {
   console.log(chalk.green(`
   ____   ___   ____       _      _   _ 
@@ -21,20 +20,16 @@ function main() {
 | |___   | |  |  _ <   / ___ \  |  _  |
  \____| |___| |_| \_\ /_/   \_\ |_| |_|                                
 `));
-  console.log(chalk.green('Developed by: Aafaq Ahmad Mir'));
+console.log(chalk.green('Developed by: Aafaq Ahmad Mir'));
   console.log(chalk.green('GitHub: github.com/miraafaq'));
   console.log(chalk.green('Website: https://miraafaq.in'));
-  
+
   rl.question(colorize('Enter the source directory (default is current directory): '), (srcDir) => {
     srcDir = srcDir || '.';
     rl.question(colorize('Enter the extensions separated by spaces (e.g., txt pdf): '), (extensions) => {
       const extArray = extensions.split(' ');
       rl.question(colorize('Enter the target directory name (will be created if not exists): '), (targetDirName) => {
         const targetDir = path.join(srcDir, targetDirName);
-        if (!fs.existsSync(targetDir)) {
-          fs.mkdirSync(targetDir, { recursive: true });
-          logMessage(`Created target directory: ${targetDir}`);
-        }
         moveFiles(srcDir, extArray, targetDir);
         rl.close();
       });
@@ -42,12 +37,11 @@ function main() {
   });
 }
 
-function colorize(text) {
-  return chalk.cyan(text);
-}
-
-if (require.main === module) {
+// Handle command line arguments for undo operation
+if (process.argv[2] === 'undo') {
+  undoLastMove();
+} else {
   main();
 }
 
-module.exports = { moveFiles, logMessage };
+module.exports = { moveFiles, logMessage, undoLastMove };
